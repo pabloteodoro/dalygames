daa1c94a6f35c6b0e3670ad42d51580c1dba32c2
Pablo Teodoro <pabloteadoro123@gmail.com>
2025-01-06 19:00:47 -0300

    Update layout.tsx
    change codes for responsive page

--- dalygames/src/app/layout.tsx
+++ dalygames/src/app/layout.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
+import { Header } from '@/components/header'
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ['latin']})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,8 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={inter.className}>
+          <Header />
         {children}
       </body>
     </html>
