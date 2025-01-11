0bf4317aa340d0c45b90aa893e202dc9222cf135
Pablo Teodoro <pabloteadoro123@gmail.com>
2025-01-11 16:57:03 -0300

    Update page.tsx

--- dalygames/src/app/page.tsx
+++ dalygames/src/app/page.tsx
@@ -6,8 +6,10 @@ async function getDalyGame(){
     const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`)
     return res.json();
 
-    }catch(err){
+    } catch(err) {
+      console.log(err);
     throw new Error("Failed to fetch data")
+      
     }
     
     
