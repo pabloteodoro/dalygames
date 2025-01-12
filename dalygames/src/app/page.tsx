dee535a27085761e91c08951e054af780d594226
Pablo Teodoro <pabloteadoro123@gmail.com>
2025-01-12 15:12:05 -0300

    create new feature
    new feature for search games and responsive site

--- dalygames/src/app/page.tsx
+++ dalygames/src/app/page.tsx
@@ -2,11 +2,13 @@ import { Container } from '../components/container/index';
 import { GameProps } from '../utils/types/game';
 import Image from 'next/image';
 import Link from 'next/link'
+import { BsArrowRightSquare } from 'react-icons/bs';
+import { Input } from '../components/input/index'
 
 
 async function getDalyGame(){
   try{
-    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`)
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`, { next: { revalidate: 320 } })
     return res.json();
 
     } catch(err) {
@@ -33,16 +35,26 @@ export default async function Home() {
         <Link href={`/game/${dalyGame.id}`}>
 
         <section className="w-full bg-black rounded-lg">
-          <Image src={dalyGame.image_url}
+          <div className='w-full max-h-96 h-96 relative rounded-lg'>
+            <div className="absolute z-20 bottom-0 p-3 flex justify-center items-center gap-2">
+            <p className='font-bold text-xl text-white'>{dalyGame.title}</p>
+            <BsArrowRightSquare size={24} color="#fff"/>
+            </div>
+            <Image
+          src={dalyGame.image_url}
           alt={dalyGame.title}
           priority={true}
           quality={100}
-          width={100}
-          height={100}
+          fill={true}
+          className="max-h-96 object-cover rounded-lg opacity-50 hover:opacity-100 transition-all duration-300"
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 33vw"
           
           
           />
-
+         
+        
+          </div>
+          
           
 
           
@@ -50,7 +62,7 @@ export default async function Home() {
         
         </Link>
 
-
+        <Input/>
 
         </Container>
 
