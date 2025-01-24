fc0f72d21f4b8a3312388f43f47a09ef283c4380
Pablo Teodoro <pabloteadoro123@gmail.com>
2025-01-24 19:30:31 -0300

    new feature
    new feature, add API in new page and view new games and yours title, descriptions and image

--- dalygames/src/app/game/[id]/page.tsx
+++ dalygames/src/app/game/[id]/page.tsx
@@ -1,9 +1,11 @@
 import { GameProps } from "@/utils/types/game";
 import Image from 'next/image'
 import { Container} from '@/components/container'
+import { Label } from './components/label'
+import { GameCard } from '@/components/GameCard'
 
 
-async function getData(id: String){
+async function getData(id: string){
 
     try {
         const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game&id={id}`)
@@ -13,7 +15,14 @@ async function getData(id: String){
     }
 } 
 
-
+async function getGameSorted(){
+    try{
+        const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`)
+        return res.json();
+    }catch(err){
+        throw new Error("Failed to fetch data")
+    }
+}
 
 
 
@@ -23,6 +32,8 @@ export default async function Game({
     params: {id: string}
 }) {
     const data: GameProps = await getData(id)
+    const sortedGame: GameProps = await getGameSorted();
+
 
     if (!data) {
        
@@ -46,10 +57,31 @@ export default async function Game({
                 <h1 className="font-bold text-x1 my-4">{data.title}</h1>
                 <p>{data.description}</p>
 
-                <h2 className="font-bold text-lg mt-7 mb-2">Categorias</h2>
-                <div>
-                    
+                <h2 className="font-bold text-lg mt-7 mb-2">Plataformas</h2>
+                <div className="flex gap-2 flex-wrap">
+                    {data.platforms.map((item) => (
+                        <Label name={item} key={item}/>
+                    ))}
                 </div>
+
+
+
+                <h2 className="font-bold text-lg mt-7 mb-2">Categorias</h2>
+                <div className="flex gap-2 flex-wrap">
+                    {data.categories.map((item) => (
+                        <Label name={item} key={item}/>
+                    ))}
+                </div>
+
+                    <p className="mt-7 mb-2"><strong>Data de Lançamento:</strong>{data.release}</p>
+
+                    <h2 className="font-bold text-lg mt-7 mb-2">Jogo Recomendado:</h2>
+                    <div className="flex">
+                        <div className="flex-grow">
+                            <GameCard data={sortedGame}/>
+                        </div>
+                    </div>
+
             </Container>
                 
 
